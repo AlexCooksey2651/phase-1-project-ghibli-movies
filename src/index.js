dfd6ab97403e7841229945359d36c86f1864b0cc
Alex Cooksey <aecooksey2651@gmail.com>
2021-12-28 18:07:04 -0500

    HTML layout basic css show movie cards

--- src/index.js
+++ src/index.js
@@ -1,21 +1,45 @@
+const movieDisplay = document.getElementById('movie-display')
+
 function fetchMoviesFromAPI() {
     fetch('https://ghibliapi.herokuapp.com/films')
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(movies => {
+        for (const movie of movies) {
+            createMovieCard(movie)
+        }
+    })
 }
 fetchMoviesFromAPI()
 
-function renderMoviesToMovieDisplay
+function createMovieCard(movie) {
+    let movieCard = document.createElement('div')
+    movieCard.className = "movie-card"
+    movieCard.id = `${movie.id}-card`
+    movieDisplay.appendChild(movieCard)
 
-function sortByRottenTomatoes
+    let movieTitle = document.createElement('h2')
+    movieTitle.className = "movie-title"
+    movieTitle.textContent = movie.title
+    movieCard.appendChild(movieTitle)
 
-function sortByRunTime
+    let movieImg = document.createElement('img')
+    movieImg.className = "movie-image"
+    movieImg.src = movie.image
+    movieCard.appendChild(movieImg)
+    
+    
+}
 
-function sortByTitle
+// function sortByRottenTomatoes
 
-function showDescription
+// function sortByRunTime
 
-function addToWatchList
+// function sortByTitle
 
-function addToAlreadySeen
+// function showDescription
+
+// function addToWatchList
+
+// function addToAlreadySeen
+
 
