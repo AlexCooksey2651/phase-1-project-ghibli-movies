4c3853c99864efa8590aa222d5d70c499b3bc476
Alex Cooksey <aecooksey2651@gmail.com>
2021-12-29 14:06:45 -0500

    Info, movie cards, watchlist, basic CSS

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const movieDisplay = document.getElementById('movie-display')
+const watchlist = document.getElementById('watchlist')
 
 function fetchMoviesFromAPI() {
     fetch('https://ghibliapi.herokuapp.com/films')
@@ -26,10 +27,42 @@ function createMovieCard(movie) {
     movieImg.className = "movie-image"
     movieImg.src = movie.image
     movieCard.appendChild(movieImg)
+
+    let releaseDate = document.createElement('p')
+    releaseDate.className = "release-date"
+    releaseDate.textContent = `Release Date: ${movie.release_date}`
+    movieCard.appendChild(releaseDate)
     
-    
+    let runTime = document.createElement('p')
+    runTime.className = "run-time"
+    runTime.textContent = `Movie Length: ${movie.running_time} minutes.`
+    movieCard.appendChild(runTime)
+
+    let rating = document.createElement('p')
+    rating.className = "movie-rating"
+    rating.textContent = `Rotten Tomatoes Score: ${movie.rt_score}%`
+    movieCard.appendChild(rating)
+
+    let watchListButton = document.createElement('button')
+    watchListButton.className = "watch-list"
+    watchListButton.innerText = "Add to Watch List"
+    watchListButton.addEventListener('click', function(evt) {
+        let movieName = evt.target.parentNode.firstChild.textContent
+        let movieBullet = document.createElement('li')
+        movieBullet.className = "watchlist-item"
+        movieBullet.innerHTML = movieName
+        watchlist.appendChild(movieBullet)
+    })
+    movieCard.appendChild(watchListButton)
 }
 
+// function addToWatchList(movie) {
+//     console.log(movie.title)
+//     let movieBullet = document.createElement('li')
+//     movieBullet.className = "watchlist-item"
+//     movieBullet.innerHTML = movie.title
+//     watchlist.appendChild(movieBullet)
+// }
 // function sortByRottenTomatoes
 
 // function sortByRunTime
